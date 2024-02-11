5cb5a38abf33f2414cd05364d4ff14dda32121ec
Denız  Kurt <denizkurt@192.168.1.33>
2024-02-11 13:46:51 +0300

    postinstall:prisma generate added to package.json

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -0,0 +1,31 @@
+import prismadb from "@/lib/prismaDb";
+
+import { SizeForm } from "./components/size-form";
+
+const SizePage = async ({
+  params
+}: {
+  params: { sizeId: string,storeId:string }
+}) => {
+  const size = await prismadb.size.findUnique({
+    where: {
+      id: params.sizeId
+    }
+  });
+
+  const categories = await prismadb.category.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+  });
+
+  return ( 
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeForm initialData={size} categories={categories} />
+      </div>
+    </div>
+  );
+}
+
+export default SizePage;
